fffcfce131e56c93ef2fe126c7f82a5226a07280
Volodymyr Trotsyshyn <volodmyr.trotsyshyn@cloudlock.com>
2018-03-21 15:40:01 +0200

    fixed packages

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytest-yield',
-    version='0.1.1',
+    version='0.1.3',
     packages=find_packages(),
     description='PyTest plugin to run tests concurrently, each `yield` switch context to other test',
     long_description=open("README.md").read(),
     author='Volodymyr Trotsyshyn',
     author_email='devova@gmail.com',
     url='https://github.com/devova/pytest-yield',
-    download_url='https://github.com/devova/pytest-yield/archive/0.1.tar.gz',
+    download_url='https://github.com/devova/pytest-yield/archive/0.1.3.tar.gz',
     py_modules=['pytest_yield'],
     install_requires=["pytest>=3.0"],
     keywords=['testing', 'pytest'],
