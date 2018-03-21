89372c13172322e769256b50a5a43594b316ebce
Volodymyr Trotsyshyn <volodmyr.trotsyshyn@cloudlock.com>
2018-03-21 15:30:47 +0200

    fixed entry_points

--- setup.py
+++ setup.py
@@ -4,24 +4,20 @@ from setuptools import setup
 
 setup(
     name='pytest-yield',
-    version='0.1',
+    version='0.1.1',
     description='PyTest plugin to run tests concurrently, each `yield` switch context to other test',
     long_description=open("README.md").read(),
     author='Volodymyr Trotsyshyn',
     author_email='devova@gmail.com',
     url='https://github.com/devova/pytest-yield',
+    download_url='https://github.com/devova/pytest-yield/archive/0.1.tar.gz',
     py_modules=['pytest_yield'],
     install_requires=["pytest>=3.0"],
-    classifiers=[
-        'Development Status :: 4 - Testing/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GPL License',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Testing'
-    ],
+    keywords=['testing', 'pytest'],
+    classifiers=[],
     entry_points={
         'pytest11': [
-            'pytest_yield = pytest_yield.plugin',
+            'yield = pytest_yield.plugin',
         ]
     }
 )
