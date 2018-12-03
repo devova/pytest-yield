34ee693fc565b532b2b5317f2403e501c7ecfda6
Volodymyr Trotsyshyn <volodmyr.trotsyshyn@cloudlock.com>
2018-12-03 15:34:13 +0200

    Bump 0.1.7

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='pytest-yield',
-    version='0.1.6',
+    version='0.1.7',
     packages=['pytest_yield'],
     description='PyTest plugin to run tests concurrently, each `yield` switch context to other test',
     long_description=open("README.md").read(),
     author='Volodymyr Trotsyshyn',
     author_email='devova@gmail.com',
     url='https://github.com/devova/pytest-yield',
-    download_url='https://github.com/devova/pytest-yield/archive/0.1.6.tar.gz',
+    download_url='https://github.com/devova/pytest-yield/archive/0.1.7.tar.gz',
     py_modules=['pytest_yield'],
     install_requires=["pytest>=3.0"],
     keywords=['testing', 'pytest'],
