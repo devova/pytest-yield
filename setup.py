cbc8ce6e5abebe9519304ef1598dfa5d2ce3fc1d
Volodymyr Trotsyshyn <volodmyr.trotsyshyn@cloudlock.com>
2018-12-04 20:19:33 +0200

    setup_scm

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup
 
 setup(
     name='pytest-yield',
-    version='0.1.9',
     packages=['pytest_yield'],
     description='PyTest plugin to run tests concurrently, each `yield` switch context to other test',
     long_description=open("README.md").read(),
@@ -12,11 +11,14 @@ setup(
     author_email='devova@gmail.com',
     use_2to3=True,
     url='https://github.com/devova/pytest-yield',
-    download_url='https://github.com/devova/pytest-yield/archive/0.1.9.tar.gz',
     py_modules=['pytest_yield'],
     install_requires=["pytest>=3.0"],
     keywords=['testing', 'pytest'],
     classifiers=[],
+    setup_requires=[
+        'setuptools_scm',
+    ],
+    use_scm_version={'root': '.', 'relative_to': __file__},
     entry_points={
         'pytest11': [
             'yield = pytest_yield.plugin',
